2c46f50598cb240342700bb5fb14785609685b80
YazykovaDaria <yazykovadasva@gmail.com>
2022-02-10 11:58:18 +0300

    add option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,8 @@ const program = new Command();
 program
 
     .version('0.0.1', '-V, --version', 'output the version number')
-    .description('Compares two configuration files and shows a difference.');
-
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format <type>', 'output format')
 program.parse();
