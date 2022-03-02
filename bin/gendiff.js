fe28a9f6b462a1fcd0b443f4c491320672cec091
YazykovaDaria <yazykovadasva@gmail.com>
2022-03-02 19:21:39 +0300

    add function

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -13,6 +13,6 @@ program
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format <type>', 'output format')
     .action((filepath1, filepath2) => {
-        // console.log(genDiff(filepath1, filepath2))
+        console.log(genDiff(filepath1, filepath2))
     })
 program.parse();
--- src/index.js
+++ src/index.js
@@ -2,42 +2,37 @@ import _ from 'lodash';
 import { readFileSync } from 'fs';
 import path from 'path';
 
-const genDiff = (filepath1, filepath2) => {
-
-};
+const readFile = (fileName) => readFileSync(path.resolve(process.cwd(), fileName), 'utf-8');
 
 const compareFiles = (file1, file2) => {
-    const allKeys = Object.keys({ ...file1, ...file2 });
+    const parse1 = JSON.parse(file1);
+    const parse2 = JSON.parse(file2);
+    const allKeys = Object.keys({ ...parse1, ...parse2 });
+    //console.log(allKeys);
     const sortKeys = _.sortBy(allKeys);
     //console.log(sort);
     const compare = sortKeys.reduce((result, key) => {
-        if (_.has(file1, key) && _.has(file2, key)) {
-            if (file1[key] === file2[key]) {
-                return result += `\r\n   ${key}: ${file1[key]}`;
+        if (_.has(parse1, key) && _.has(parse2, key)) {
+            if (parse1[key] === parse2[key]) {
+                return result += `\r\n   ${key}: ${parse1[key]}`;
             }
-            return result += `\r\n - ${key}: ${file1[key]}\r\n + ${key}: ${file2[key]}`;
-        } if (!file1.hasOwnProperty(key) && file2.hasOwnProperty(key)) {
-            return result += `\r\n + ${key}: ${file2[key]}`;
+            return result += `\r\n - ${key}: ${parse1[key]}\r\n + ${key}: ${parse2[key]}`;
+        } if (!parse1.hasOwnProperty(key) && parse2.hasOwnProperty(key)) {
+            return result += `\r\n + ${key}: ${parse2[key]}`;
         }
-        return result += `\r\n - ${key}: ${file1[key]}`;
+        return result += `\r\n - ${key}: ${parse1[key]}`;
     }, '{')
     const result = `${compare}\r\n}`;
-    console.log(result);
+    //console.log(result);
     return result;
-}
-// const f = {
-//     "host": "hexlet.io",
-//     "timeout": 50,
-//     "proxy": "123.234.53.22",
-//     "follow": false
-// };
+};
 
-// const j = {
-//     "timeout": 20,
-//     "verbose": true,
-//     "host": "hexlet.io"
-// };
-
-//compareFiles(f, j);
+const genDiff = (filepath1, filepath2) => {
+    const contentFile1 = readFile(filepath1);
+    const contentFile2 = readFile(filepath2);
+    //console.log(contentFile1);
+    const compare = compareFiles(contentFile1, contentFile2);
+    return compare;
+};
 
 export default genDiff;
