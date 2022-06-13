6acc4120f4eb5edf47c0a81c284624ed4f0ed58e
YazykovaDaria <yazykovadasva@gmail.com>
2022-06-13 22:27:25 +0300

    change logic render

--- src/render.js
+++ src/render.js
@@ -10,38 +10,43 @@ const getIndicator = (indicator) => {
       return '  ';
     default:
       return '*';
-    //throw new Error('incorrect indicator');
+    // throw new Error('incorrect indicator');
   }
 };
 
 const render = (collection) => {
   const item = (value, depth) => {
-
+    const replacer = ' ';
+    const spacesCount = 2;
+    const indentSize = depth * spacesCount;
+    const currentIndent = replacer.repeat(indentSize);
+    const bracketIndent = replacer.repeat(indentSize - spacesCount);
     const result = value.flatMap((obj) => {
-      const replacer = ' ';
-      const spacesCount = 2;
-      const indentSize = depth * spacesCount;
-      const currentIndent = replacer.repeat(indentSize);
-      const bracketIndent = replacer.repeat(indentSize - spacesCount);
-
-      const keys = Object.entries(obj)
-        .flatMap(([key, val]) => {
-          const { indicator } = key;
-          const keyStr = `${currentIndent}${getIndicator(indicator)}${[key]}`;
-          if (!isObject(val)) {
-            return `${keyStr}: ${val}`;
+      const { indicator } = obj;
+      //console.log(indicator);
+      const keys = Object.keys(obj)
+        .flatMap((key) => {
+          // const [currentKey, indicator] = key;
+          if (key !== 'indicator') {
+            //console.log(obj[key]);
+            const keyStr = `${currentIndent}${getIndicator(indicator)}${[key]}`;
+            if (!Array.isArray(obj[key])) {
+              return `${keyStr}: ${obj[key]}`;
+            }
+            //console.log(obj[key])
+            return `${keyStr}: ${item(obj[key], depth + 1)}`;
           }
-          // разобраться с формированием строк (просмотреть какие кл-зн получаю) и индикаторами!
-          return `${keyStr}: ${item(val, depth + 1)}`;
+          return [];
         });
-      const y = ['{', ...keys, `${bracketIndent}}`].join('\n');
+
       //console.log(y);
-      return y;
+      return keys;
     });
-    return result;
+    const y = ['{', ...result, `${bracketIndent}}`].join('\n');
+    return y;
   };
   const f = item(collection, 1);
-  console.log(f);
+  //console.log(f);
   return f;
 };
 
@@ -51,6 +56,7 @@ const o1 = {
   key3: {
     nextK1: 2,
     nextK2: {
+      // вот эта вложенность не прокатывает, у меня тут объект без индикаторов, как распаковать?
       has: {
         keg: 1,
         h: 2,
@@ -73,6 +79,6 @@ const o2 = {
 };
 // console.log(getDifferents(o1, o2));
 const h = getDifferents(o1, o2);
-//console.log(render(h));
+//console.log(h);
 render(h);
 export default render;
