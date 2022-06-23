19f94772f29e8c6742f164c619c4eefed2251675
YazykovaDaria <yazykovadasva@gmail.com>
2022-06-23 12:47:26 +0300

    change logic

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,71 +1,35 @@
-import getDifferents from '../getDiff.js';
-
 const isComplexValue = (value) => typeof value === 'object' && value !== null;
 
 const valueStr = (val) => {
-    if (isComplexValue(val)) {
-        return '[complex value]';
-    }
-    return typeof val === 'boolean' || val === 'null' ? val : `'${val}'`;
-};
-
-const buildStr = (diff, name) => {
-    const { state } = diff;
-    switch (state) {
-        case 'update':
-            return `Property '${name}' was updated. From ${valueStr(diff.firstVal)} to ${valueStr(diff.secondVal)}`;
-        case 'added':
-            return `Property '${name}' was added with value: ${valueStr(diff.value)}`;
-        case 'deleted':
-            return `Property '${name}' was removed`;
-        default:
-            throw new Error('incorrect indicator');
-    }
+  if (isComplexValue(val)) {
+    return '[complex value]';
+  }
+  return typeof val === 'boolean' || val === null ? val : `'${val}'`;
 };
 
 const plain = (collection) => {
-    const item = (coll, name = '') => {
-        // console.log(coll);
-        const result = coll.filter((obj) => obj.state !== 'identic')
-            .flatMap((obj) => {
-                //console.log(obj);
-                const { state } = obj;
-                const nextName = `${name}${obj.key}`;
-                if (state === 'interior') {
-                    return item(obj.children, `${nextName}.`);
-                }
-                return buildStr(obj, nextName);
-            });
-        // console.log(result.join('\n'));
-        return result.join('\n');
-    };
-    return item(collection);
+  const item = (coll, name = '') => {
+    const result = coll.filter((obj) => obj.state !== 'identic')
+      .flatMap((obj) => {
+        const { state } = obj;
+        const nextName = `${name}${obj.key}`;
+        if (state === 'interior') {
+          return item(obj.children, `${nextName}.`);
+        }
+        switch (state) {
+          case 'update':
+            return `Property '${nextName}' was updated. From ${valueStr(obj.firstVal)} to ${valueStr(obj.secondVal)}`;
+          case 'added':
+            return `Property '${nextName}' was added with value: ${valueStr(obj.value)}`;
+          case 'deleted':
+            return `Property '${nextName}' was removed`;
+          default:
+            throw new Error('incorrect state');
+        }
+      });
+    return result.join('\n');
+  };
+  return item(collection);
 };
-
-//const nextName = `${name}${key}`;
-//return item(obj[key], `${nextName}.`);
-const a = {
-    key: {
-        next1: 'h',
-        next2: {
-            n: 3,
-        },
-    },
-};
-
-const b = {
-    key: {
-        next1: 'hi',
-        next2: {
-            n: 2,
-        },
-        next3: 'net',
-    },
-};
-
-//const y = getDifferents(a, b);
-// console.log(JSON.stringify(y));
-// plain(y);
-//console.log(plain(y));
 
 export default plain;
