7481fd09cc226316264b973a2e7eada5cc33c184
YazykovaDaria <yazykovadasva@gmail.com>
2022-06-23 12:50:08 +0300

    delete comments

--- src/getDiff.js
+++ src/getDiff.js
@@ -7,15 +7,6 @@ const isObject = (value) => {
   return false;
 };
 
-const buildDiff = (key, value, marker = 'none') => {
-  if (!isObject(value)) {
-    return { [key]: value, indicator: marker };
-  }
-  const result = Object.keys(value)
-    .map((currentKey) => (buildDiff(currentKey, value[currentKey])));
-  return { [key]: result, indicator: marker };
-};
-
 const getDifferents = (obj1, obj2) => {
   const allKeys = _.union(Object.keys(obj1).concat(Object.keys(obj2)));
   const sortKeys = allKeys.sort();
@@ -23,23 +14,17 @@ const getDifferents = (obj1, obj2) => {
   const differents = sortKeys.flatMap((key) => {
     if (_.has(obj1, key) && !_.has(obj2, key)) {
       return { key, state: 'deleted', value: obj1[key] };
-      //buildDiff(key, obj1[key], 'onlyFirst');
     } if (!_.has(obj1, key) && _.has(obj2, key)) {
       return { key, state: 'added', value: obj2[key] }
-      //buildDiff(key, obj2[key], 'onlySecond');
     }
-    // if (obj1[key] === obj2[key]) {
-    //   return buildDiff(key, obj1[key], 'notDiff');
-    // }
     if (isObject(obj1[key]) && isObject(obj2[key])) {
       return { key, children: getDifferents(obj1[key], obj2[key]), state: 'interior' };
-      //buildDiff(key, getDifferents(obj1[key], obj2[key]), 'notDiff');
     } if (obj1[key] !== obj2[key]) {
       return { key, state: 'update', firstVal: obj1[key], secondVal: obj2[key] };
-      //[buildDiff(key, obj1[key], 'onlyFirst'), buildDiff(key, obj2[key], 'onlySecond')];
     }
     return { key, value: obj1[key], state: 'identic' };
   });
+  console.log(differents);
   return differents;
 };
 
