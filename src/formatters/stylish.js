48e6214d71aa945fb47131d0acc24c1915703a58
YazykovaDaria <yazykovadasva@gmail.com>
2022-06-24 15:18:42 +0300

    change logic

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -17,7 +17,7 @@ const isObject = (value) => {
 
 const amountSpace = 4;
 const replaser = ' ';
-const getSpace = (deep, spaces = 2) => replaser.repeat(deep * amountSpace - spaces);
+const getSpace = (deep, spaces = 2) => replaser.repeat(deep * amountSpace);
 // ёбаные пробелы!!!
 
 const stylish = (collection) => {
@@ -36,50 +36,50 @@ const stylish = (collection) => {
 
     const lines = coll.flatMap((obj) => {
       const { state } = obj;
-      console.log(state, obj);
+      //console.log(state, obj);
       const startStr = `${getSpace(depth)}${indicators[state]}${obj.key}: `;
       if (state === 'interior') {
         return `${startStr}${item(obj.children, depth + 1)}`;
       } if (state === 'update') {
-        return [`${startStr}${valueStr(obj.firstVal)}`, `${startStr}${valueStr(obj.secondVal)}`];
+        return [`${getSpace(depth)}${indicators.deleted}${obj.key}: ${valueStr(obj.firstVal)}`, `${getSpace(depth)}${indicators.added}${obj.key}: ${valueStr(obj.secondVal)}`];
       }
       return `${startStr}${valueStr(obj.value)}`;
     });
     const result = ['{', ...lines, `${getSpace(depth - 1)}}`].join('\n');
-    //console.log(result);
     return result;
   };
-  return item(collection, 2);
+  return item(collection, 1);
 };
 
 export default stylish;
-const a = {
-  key1: {
-    next1: 'hi',
-    // n: 3,
-    next11: {
-      n: 3,
-    },
-  },
-};
 
-const b = {
-  key2: {
-    next2: 'h',
-    next22: {
-      n: 2,
-    },
-    next3: 'net',
-  },
-};
-const m = [
-  { key: 'follow', state: 'added', value: false },
-  { key: 'setting1', value: 'Value 1', state: 'identic' },
-  { key: 'setting2', state: 'deleted', value: 200 },
-  { key: 'setting3', state: 'update', firstVal: true, secondVal: null },
-  { key: 'setting4', state: 'added', value: 'blah blah' },
-  { key: 'setting5', state: 'added', value: { key5: 'value5' } },
-]
+// const a = {
+//   key1: {
+//     next1: 'hi',
+//     // n: 3,
+//     next11: {
+//       n: 3,
+//     },
+//   },
+// };
+
+// const b = {
+//   key2: {
+//     next2: 'h',
+//     next22: {
+//       n: 2,
+//     },
+//     next3: 'net',
+//   },
+// };
+// const m = [
+//   { key: 'follow', state: 'added', value: false },
+//   { key: 'setting1', value: 'Value 1', state: 'identic' },
+//   { key: 'setting2', state: 'deleted', value: 200 },
+//   { key: 'setting3', state: 'update', firstVal: true, secondVal: null },
+//   { key: 'setting4', state: 'added', value: 'blah blah' },
+//   { key: 'setting5', state: 'added', value: { key5: 'value5' } },
+// ]
 // const y = getDifferents(a, b);
-console.log(stylish(m));
+//console.log(stylish(m));
 // console.log(y);
