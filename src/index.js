77fd12beedc7a6d9aa67cab0777b4980e0916382
YazykovaDaria <yazykovadasva@gmail.com>
2022-06-24 15:16:59 +0300

    add default format

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import { readFileSync } from 'fs';
 import path from 'path';
-//import render from './render.js';
 import parser from './parser.js';
 import getDifferents from './getDiff.js';
-import plain from './formatters/plain.js';
+import render from './formatters/index.js';
 
 const readFile = (fileName) => readFileSync(path.resolve(process.cwd(), fileName), 'utf-8');
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, format) => {
+  console.log(format);
   const contentFile1 = readFile(filepath1);
   const contentFile2 = readFile(filepath2);
   const obj1 = parser(path.extname(filepath1), contentFile1);
   const obj2 = parser(path.extname(filepath2), contentFile2);
   const differents = getDifferents(obj1, obj2);
-  const diff = plain(differents);
+  const diff = render(differents, format);
   return diff;
 };
 
