485d633eaceeae0d9a52b4480f890b24455f77a7
YazykovaDaria <yazykovadasva@gmail.com>
2022-07-01 11:16:46 +0300

    change isObj

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,12 +1,5 @@
 import _ from 'lodash';
 
-export const isObject = (value) => {
-  if (typeof value === 'object' && !Array.isArray(value) && value !== null) {
-    return true;
-  }
-  return false;
-};
-
 const getDifferents = (obj1, obj2) => {
   const allKeys = _.union(Object.keys(obj1), Object.keys(obj2));
   const sortKeys = _.sortBy(allKeys);
@@ -17,7 +10,7 @@ const getDifferents = (obj1, obj2) => {
     } if (!_.has(obj1, key) && _.has(obj2, key)) {
       return { key, state: 'added', value: obj2[key] };
     }
-    if (isObject(obj1[key]) && isObject(obj2[key])) {
+    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
       return { key, children: getDifferents(obj1[key], obj2[key]), state: 'interior' };
     } if (obj1[key] !== obj2[key]) {
       return {
