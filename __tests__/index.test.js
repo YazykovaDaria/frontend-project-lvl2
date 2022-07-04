c75e248e232c0516e9b41f14c11cd9e72c90aa12
YazykovaDaria <yazykovadasva@gmail.com>
2022-07-04 22:22:07 +0300

    add defaultFormat test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,19 +7,21 @@ import genDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const pathToTestFile = (fileName) => path.join(__dirname, '__fixtures__', fileName);
+const pathToTestFile = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
 
 const getCorrectAnswers = (arr) => arr.map((filename) => readFileSync(pathToTestFile(filename), 'utf-8'));
 
-const correctAnswers = getCorrectAnswers(['stylish.txt', 'plain.txt', 'json.txt']);
+const [stylish, plain, json] = getCorrectAnswers(['stylish.txt', 'plain.txt', 'json.txt']);
 
 test.each(['yaml', 'json'])('diff', (ext) => {
   const path1 = pathToTestFile(`first.${ext}`);
   const path2 = pathToTestFile(`second.${ext}`);
+  const testDefaultFormat = genDiff(path1, path2);
+  expect(testDefaultFormat).toBe(stylish);
   const testStylish = genDiff(path1, path2, 'stylish');
-  expect(testStylish).toBe(correctAnswers[0]);
+  expect(testStylish).toBe(stylish);
   const testPlain = genDiff(path1, path2, 'plain');
-  expect(testPlain).toBe(correctAnswers[1]);
+  expect(testPlain).toBe(plain);
   const testJson = genDiff(path1, path2, 'json');
-  expect(testJson).toBe(correctAnswers[2]);
+  expect(testJson).toBe(json);
 });
